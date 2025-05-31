d75f2741d1b5a1cd4f9796a4adbf9092b2bb2f11
Davi Bauer <davibauer@gmail.com>
2025-05-31 17:44:38 -0300

    fix: update defaultSnapOrigin to remove version specifier

--- packages/site/src/config/snap.ts
+++ packages/site/src/config/snap.ts
@@ -3,4 +3,4 @@
  * Will default to the local hosted snap.
  * Change it once you have the url where the snap is hosted.
  */
-export const defaultSnapOrigin = `npm:@yarulabs/coti-snap@latest`;
+export const defaultSnapOrigin = `npm:@yarulabs/coti-snap`;
